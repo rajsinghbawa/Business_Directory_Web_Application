c1485ab358e4418dd6dde09a2544332edc412304
rajsinghbawa <168783118+rajsinghbawa@users.noreply.github.com>
2025-04-11 21:15:08 -0400

    Delete frontend directory

--- frontend/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { getToken } from '../auth';
-
-const PrivateRoute = ({ children }) => {
-  return getToken() ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
